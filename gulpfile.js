d5ce84babf0eb9e43dc4a62b43e9d93d9c2bd6d9
AntonGorban <61562068+AntonGorban@users.noreply.github.com>
2020-09-12 23:03:02 +0300

    formatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 "use strict";
-/* jshint node: true */		// For JShint
-/* jshint esversion: 6 */	// For JShint
+/* jshint node: true */ // For JShint
+/* jshint esversion: 6 */ // For JShint
 
 // Settings
-const coloring	= true,	// on/off coloring of logs
-	voice		= true;	// on/off voice action
+const
+	coloring	= true,		// on/off coloring of logs
+	voice		= true,		// on/off voice action
+	notifies	= true,		// on/off notifications
+	preprocHTML	= 'pug',	// HTML preprocessor
+	preprocCSS	= 'scss',	// CSS preprocessor
+	vCSS		= false,	// Will vendor CSS be used
+	vJS			= false;	// Will vendor JS be used
 
 // Colors
-const reset	= "\x1b[0m",
+const
+	reset	= "\x1b[0m",
 	black	= "\x1b[30m",
 	red		= "\x1b[31m",
 	green	= "\x1b[32m",
@@ -18,233 +25,240 @@ const reset	= "\x1b[0m",
 	white	= "\x1b[37m";
 
 // Libraries
-const {
-	src,
-	dest,
-	parallel,
-	series
-} = require("gulp"),
+const
+	{src, dest, parallel, series }	= require("gulp"),
 	browserSync	= require("browser-sync").create(),	// Live server
 	reload		= browserSync.reload,				// Reloading server
 	bowerFiles	= require("main-bower-files"),		// Parsing bower packages
 	watch		= require("gulp-watch"),			// Watching files
-	prefixer	= require("gulp-autoprefixer"),		// Autoprefixer
-	uglify		= require("gulp-uglify"), 			// JS minimize
-	sourcemaps	= require("gulp-sourcemaps"),		// Generate sourcemaps
-	sass		= require("gulp-sass"),				// SCSS
 	pug			= require("gulp-pug"),				// PUG
+	rename		= require("gulp-rename"),			// Renaming files
+	sass		= require("gulp-sass"),				// SCSS
+	prefixer	= require("gulp-autoprefixer"),		// Autoprefixer
+	sourcemaps	= require("gulp-sourcemaps"),		// Generate sourcemaps
 	cleanCSS	= require("gulp-clean-css"),		// CSS minimize
+	uglify		= require("gulp-uglify"),			// JS minimize
 	imagemin	= require("gulp-imagemin"),			// Img minimize
+	changed		= require("gulp-changed"),			// Parsing newer files (code)
+	newer		= require("gulp-newer"),			// Parsing newer files (images)
 	rimraf		= require("rimraf"),				// Clean
+	gulpif		= require("gulp-if"),				// 'If' in .pipe
 	fs			= require("fs-extra"),				// Create files
-	say			= require("say"),					// Voice action
-	newer		= require("gulp-newer");			// Parsing newer files
+	notify		= require("gulp-notify"),			// Notifications
+	say			= require("say");					// Voice action
 
 // Path's
 const path = {
 	vendor: {	// Bower packages
-		js: "app/js/",
-		css: "app/css/",
+		js:		"app/js/",
+		css:	"app/css/",
 	},
 	dist: {		// Build files
-		html: "dist/",
-		js: "dist/js/",
-		scss: "dist/css/",
-		css: "dist/css/",
-		img: "dist/img/",
-		fonts: "dist/fonts/",
+		html:	"dist/",
+		js:		"dist/js/",
+		scss:	"dist/css/",
+		css:	"dist/css/",
+		img:	"dist/img/",
+		fonts:	"dist/fonts/",
 	},
 	app: {		// Development files
-		pug: "app/**/*.pug",
-		html: "app/**/*.html",
-		js: "app/js/*.js",
-		scss: "app/css/*.scss",
-		css: "app/css/*.css",
-		img: "app/img/**/*.*",
-		fonts: "app/fonts/**/*.*",
+		pug:	"app/**/*.pug",
+		html:	"app/**/*.html",
+		js:		"app/js/*.js",
+		scss:	"app/css/*.scss",
+		css:	"app/css/*.css",
+		img:	"app/img/**/*.*",
+		fonts:	"app/fonts/**/*.*",
 	},
 	watch: {	// Files for watching
-		pug: "app/**/*.pug",
-		html: "app/**/*.html",
-		js: "app/js/**/*.js",
-		scss: "app/css/**/*.scss",
-		css: "app/css/**/*.css",
-		img: "app/img/**/*.*",
-		fonts: "app/fonts/**/*.*",
+		pug:	"app/**/*.pug",
+		html:	"app/**/*.html",
+		js:		"app/js/**/*.js",
+		scss:	"app/css/**/*.scss",
+		css:	"app/css/**/*.css",
+		img:	"app/img/**/*.*",
+		fonts:	"app/fonts/**/*.*",
 	},
 	clean: "./dist",	// Files for cleaning
 };
 
-function color(code) {
-	// Coloring of logs
+const config = {			// Initializing the browser-sync package
+	server: {				// Server directory
+		baseDir: "dist/"
+	},
+	logPrefix: "antogor",	// Server name
+	online: true,			// Work in online / offline
+	ghostMode: false		// Displaying actions on all devices
+};
+
+function color(code) {		// Coloring of logs
 	if (coloring) console.log(code);
 }
 
-function speak(message) {
-	// Voice action
+function speak(message) {	// Voice action
 	if (voice) say.speak(message);
 }
 
-function server() {
-	// Server
-	color(cyan); // Coloring logs
-	browserSync.init({
-		// Initializing the browser-sync package
-		server: {
-			baseDir: "dist/"
-		}, // Server directory
-		online: true, // Work in online / offline
-		ghostMode: false, // Displaying actions on all devices
-		LogPrefix: "antogor", // Server name
-	});
-	speak("server started"); // Voice
+function notice(message) {	// Notifications
+	if (notifies) notify(message);
+}
+
+function server() {				// Server
+	color(cyan);				// Coloring logs
+	browserSync.init(			// Starting server
+		config
+	);
+	notice("Server started");	// Notification
+	speak("server started");	// Voice
 }
 
 function vendorCSS() {
-	return src(bowerFiles("**/*.css")) // Take vendor files
-		.pipe(newer(path.vendor.css)) // Select newer files
-		.pipe(dest(path.vendor.css)); // Drop in dev
+	if (vCSS) {
+		return src(bowerFiles("**/*.css"))	// Take vendor files
+			.pipe(changed(path.vendor.css))	// Select newer files
+			.pipe(dest(path.vendor.css));	// Drop in dev
+	} else {
+		console.log(magenta, "vendor CSS do not be used", reset);
+	}
 }
 
 function vendorJS() {
-	return src(bowerFiles("**/*.js")) // Take vendor files
-		.pipe(newer(path.vendor.js)) // Select newer files
-		.pipe(dest(path.vendor.js)); // Drop in dev
+	if (vJS) {
+		return src(bowerFiles("**/*.js"))	// Take vendor files
+			.pipe(changed(path.vendor.js))	// Select newer files
+			.pipe(dest(path.vendor.js));	// Drop in dev
+	} else {
+		console.log(magenta, "vendor JS do not be used", reset);
+	}
 }
 
 function build_PUG() {
-	return src(path.app.pug) // Take PUG files
-		.pipe(newer(path.dist.html)) // Select newer files
-		.pipe(pug({
+	return src(path.app.pug)			// Take PUG files
+		.pipe(changed(path.dist.html,	// Select newer files
+			{extension: ".html"
+		}))
+		.pipe(pug({						// Convert in HTML
 			pretty: false
-		})) // Convert in HTML
-		.on("error", function () {
-			// At error
-			color(red); // Coloring logs
-			speak("Invalid PUG"); // Voice
+		}))
+
+		.on("error", function () {		// At error
+			color(red);					// Coloring logs
+			notice("Invalid PUG");		// Notification
+			speak("Invalid PUG");		// Voice
 		})
-		.on("error", console.log) // Log error
+		.on("error", console.log)		// Log error
 		.on("error", function () {
-			color(reset); // Reset color logs
+			color(reset);				// Reset color logs
 		})
-		.pipe(dest(path.dist.html)) // Drop in build
-		.pipe(reload({
+
+		.pipe(dest(path.dist.html))		// Drop in build
+		.pipe(reload({					// Reload server
 			stream: true
-		})); // Reload server
+		}));
 }
 
 function build_HTML() {
-	return src(path.app.html) // Take HTML files
-		.pipe(newer(path.dist.html)) // Select newer files
-		.pipe(dest(path.dist.html)) // Drop in build
-		.pipe(reload({
+	return src(path.app.html)			// Take HTML files
+		.pipe(changed(path.dist.html))	// Select newer files
+		.pipe(dest(path.dist.html))		// Drop in build
+		.pipe(reload({					// Reload server
 			stream: true
-		})); // Reload server
+		}));
 }
 
 function build_SCSS() {
-	return src(path.app.scss) // Take SCSS files
-		.pipe(newer(path.dist.css)) // Select newer files
-		.pipe(sourcemaps.init()) // Initializing sourcemaps
-		.pipe(sass()) // Convert in CSS
+	return src(path.app.scss)			// Take SCSS files
+		.pipe(changed(path.dist.css, {	// Select newer files
+			extension: ".css"
+		}))
+		.pipe(sourcemaps.init())		// Initializing sourcemaps
+		.pipe(sass())					// Convert in CSS
+
 		.on("error", function () {
 			// At error
-			color(red); // Coloring logs
-			speak("Invalid SCSS"); // Voice
+			color(red);					// Coloring logs
+			notice("Invalid SCSS");		// Notification
+			speak("Invalid SCSS");		// Voice
 		})
-		.on("error", console.log) // Log error
+		.on("error", console.log)		// Log error
 		.on("error", function () {
-			color(reset); // Reset color logs
+			color(reset);				// Reset color logs
 		})
-		.pipe(
-			prefixer({
-				// Add prefixes
-				overfideBrowserslist: ["last 10 versions"],
-				grid: true,
-			})
-		)
-		.pipe(
-			cleanCSS({
-				// Minimize CSS
-				level: {
-					1: {
-						specialComments: 0
-					}
-				},
-			})
-		)
-		.pipe(sourcemaps.write()) // Add sourcemaps
-		.pipe(dest(path.dist.css)) // Drop in build
-		.pipe(reload({
+
+		.pipe(prefixer({				// Add prefixes
+			overfideBrowserslist: ["last 10 versions"],
+			grid: true,
+		}))
+		.pipe(cleanCSS({				// Minimize CSS
+			level: {
+				1: {
+					specialComments: 0
+				}
+			},
+		}))
+		.pipe(sourcemaps.write())		// Add sourcemaps
+		.pipe(dest(path.dist.css))		// Drop in build
+		.pipe(reload({					// Reload server
 			stream: true
-		})); // Reload server
+		}));
 }
 
 function build_CSS() {
-	return src(path.app.css) // Take SCSS files
-		.pipe(newer(path.dist.css)) // Select newer files
-		.pipe(sourcemaps.init()) // Initializing sourcemaps
-		.pipe(
-			prefixer({
-				// Add prefixes
-				overfideBrowserslist: ["last 10 versions"],
-				grid: true,
-			})
-		)
-		.pipe(
-			cleanCSS({
-				// Minimize CSS
-				level: {
-					1: {
-						specialComments: 0
-					}
-				},
-			})
-		)
-		.pipe(sourcemaps.write()) // Add sourcemaps
-		.pipe(dest(path.dist.css)) // Drop in build
-		.pipe(reload({
+	return src(path.app.css)			// Take SCSS files
+		.pipe(changed(path.dist.css))	// Select newer files
+		.pipe(sourcemaps.init())		// Initializing sourcemaps
+		.pipe(prefixer({				// Add prefixes
+			overfideBrowserslist: ["last 10 versions"],
+			grid: true,
+		}))
+		.pipe(cleanCSS({				// Minimize CSS
+			level: {
+				1: {
+					specialComments: 0
+				}
+			},
+		}))
+		.pipe(sourcemaps.write())		// Add sourcemaps
+		.pipe(dest(path.dist.css))		// Drop in build
+		.pipe(reload({					// Reload server
 			stream: true
-		})); // Reload server
+		}));
 }
 
 function build_JS() {
-	return src(path.app.js) // Take JS files
-		.pipe(newer(path.dist.js)) // Select newer files
-		.pipe(sourcemaps.init()) // Initializing sourcemaps
-		.pipe(uglify()) // Minimize JS
-		.pipe(sourcemaps.write()) // Add sourcemaps
-		.pipe(dest(path.dist.js)) // Drop in build
-		.pipe(reload({
+	return src(path.app.js)				// Take JS files
+		.pipe(changed(path.dist.js))	// Select newer files
+		.pipe(sourcemaps.init())		// Initializing sourcemaps
+		.pipe(uglify())					// Minimize JS
+		.pipe(sourcemaps.write())		// Add sourcemaps
+		.pipe(dest(path.dist.js))		// Drop in build
+		.pipe(reload({					// Reload server
 			stream: true
-		})); // Reload server
+		}));
 }
 
 function build_img() {
-	return src(path.app.img) // Take img files
-		.pipe(newer(path.dist.img)) // Select newer files
-		.pipe(
-			imagemin({
-				// Minimize img
-				progressive: true,
-				svgoPlugins: [{
-					removeViewBox: false
-				}],
-				interlaced: true,
-			})
-		)
-		.pipe(dest(path.dist.img)) // Drop in build
-		.pipe(reload({
+	return src(path.app.img)			// Take img files
+		.pipe(newer(path.dist.img))		// Select newer files
+		.pipe(imagemin({				// Minimize img
+			progressive: true,
+			svgoPlugins: [{
+				removeViewBox: false
+			}],
+			interlaced: true,
+		}))
+		.pipe(dest(path.dist.img))		// Drop in build
+		.pipe(reload({					// Reload server
 			stream: true
-		})); // Reload server
+		}));
 }
 
 function build_fonts() {
-	return src(path.app.fonts) // Take fonts files
-		.pipe(newer(path.dist.fonts)) // Select newer files
-		.pipe(reload({
+	return src(path.app.fonts)			// Take fonts files
+		.pipe(newer(path.dist.fonts))	// Select newer files
+		.pipe(reload({					// Reload server
 			stream: true
-		})); // Reload server
+		}));
 }
 
 function watching() {
@@ -290,10 +304,11 @@ function directory() {
 }
 
 function files() {
-	fs.writeFile('./app/index.pug', '-var title = ""\n-var description = ""\n-var keywords = ""\n-var og_img = "img/"\n-var url = "https:\u002F\u002F"\n\ndoctype html\nhtml(lang="ru")\n  head\n    \u002F\u002F- About document\n    meta(charset="UTF-8")\n    meta(name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0")\n    meta(name="description" content=description)\n    meta(name="keywords" content=keywords)\n    title= title\n    \u002F\u002F- Icons\n    link(rel="icon" href="img/ico.ico")\n    link(rel="shortcut icon" href="img/ico.ico")\n    \u002F\u002F- OG\n    meta(property="og:title" content=title)\n    meta(property="og:type" content="website")\n    meta(property="og:image" content=og_img)\n    meta(property="og:url" content=url)\n    meta(property="og:description" content=description)\n    \u002F\u002F- Styles\n    link(rel="stylesheet" href="css/main.css")\n  body\n    \n    script(src="js/main.js")\n');
+	fs.writeFile('./app/index.pug', '-var title = ""\n-var description = ""\n-var keywords = ""\n-var og_img = "img/"\n-var url = "https:\u002F\u002F"\n\ndoctype html\nhtml(lang="ru")\n	head\n		\u002F\u002F- About document\n		meta(charset="UTF-8")\n		meta(name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0")\n		meta(name="description" content=description)\n		meta(name="keywords" content=keywords)\n		title= title\n		\u002F\u002F- Icons\n		link(rel="icon" href="img/ico.ico")\n		link(rel="shortcut icon" href="img/ico.ico")\n		\u002F\u002F- OG\n		meta(property="og:title" content=title)\n		meta(property="og:type" content="website")\n		meta(property="og:image" content=og_img)\n		meta(property="og:url" content=url)\n		meta(property="og:description" content=description)\n		\u002F\u002F- Styles\n		link(rel="stylesheet" href="css/main.css")\n	body\n		\n		script(src="js/main.js")');
 	fs.writeFile("./app/css/main.scss", "");
 	fs.writeFile("./app/css/media.scss", "@media screen and (max-width: 991px) {\n	\n}\n");
 	fs.writeFile("./app/js/main.js", "$(document).ready(function(){\n	\n});");
+	notice("Files created");
 	console.log(magenta, "~ Files created ~", reset);
 }
 
